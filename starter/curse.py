969ff7751848ee2673bdf914a28b5732bdd0444c
sunnysarkar <sarkarsunny29@gmail.com>
2025-11-23 20:47:04 -0800

    Test curse more

--- starter/curse.py
+++ starter/curse.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import make_classification
 import numpy as np
 
-import random
+import math
 
 # Create figure and styling for plotting
 fig, ax = plt.subplots(1, 1, figsize=(6, 3))
@@ -11,27 +11,44 @@ line_styles = {0: 'ro-', 1: 'b^-', 2: 'gs-', 3: 'cv-'}
 
 # Plot dmax/dmin ratio
 # TODO: fill in valid test numbers
-for idx, num_samples in enumerate([50, 100, 500, 1000]):
+for idx, num_samples in enumerate([5, 10, 50, 100]):
     # TODO: Fill in a valid feature range
     feature_range = range(1, 101)
     ratios = []
+    dat = []
     for num_features in feature_range:
         # TODO: Generate synthetic data using make_classification
-        X, _ = make_classification(n_samples=num_samples, n_features=num_features, random_state=1)
+        X, _ = make_classification(n_samples=num_samples, n_features=num_features, n_informative=1, n_redundant=0, n_clusters_per_class=1)
+
 
         # TODO: Choose random query point from X
-        query_idx = random.randint(0, num_samples-1)
-        query_point = X[query_idx]
+        query_point = X[-1]
 
         # TODO: remove query pt from X so it isn't used in distance calculations
+        X = X[:-1]
 
         # TODO: Calculate distances
-        distances = None
+        distances = []
+        for point in X:
+            dist = point - query_point
+            eucluidian_dist = 0
+            for coord in dist:
+                eucluidian_dist += (coord * coord)
+            distances.append(math.sqrt(eucluidian_dist))
+
+        # distances = [np.linalg.norm(point - query_point) for point in X]
         ratio = np.max(distances) / np.min(distances)
         ratios.append(ratio)
+        dat.append((ratio, np.max(distances), np.min(distances)))
+
+
+    print([x[1] for x in dat])
+    import pdb; pdb.set_trace()
 
     ax.plot(feature_range, np.log(ratios), line_styles[idx], label=f'N={num_samples:,}')
 
 plt.legend()
 plt.tight_layout()
 plt.grid(True)
+
+plt.show()
