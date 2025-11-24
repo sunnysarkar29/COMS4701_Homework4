b0e655518bf858706d50b84f04a967c8c68bbc07
sunnysarkar <sarkarsunny29@gmail.com>
2025-11-23 16:35:56 -0800

    Part 1: Classification Code Complete

--- starter/classifiers.py
+++ starter/classifiers.py
@@ -31,7 +31,6 @@ class Classifiers():
         plt.title('Part A: Data Visualization')
         plt.xlabel('A')
         plt.ylabel('B')
-        plt.legend(['A', 'B'])
         plt.savefig('partA.png')
         plt.show()
         # plt.show(block=False)
@@ -86,12 +85,6 @@ class Classifiers():
     def test_clf(self, clf, classifier_name=''):
         # TODO: Fit the classifier and extrach the best score, training score and parameters
         clf.fit(self.training_data, self.training_labels)
-        # print(clf.cv_results_)
-
-        # print(clf.cv_results_['mean_test_score'])
-        # print(clf.best_score_)
-        # print(clf.best_params_)
-        # print(clf.best_estimator_)
 
         trainingScore = clf.best_score_
 
@@ -100,12 +93,9 @@ class Classifiers():
 
         clf = clf.best_estimator_
         testingScore = clf.score(self.testing_data, self.testing_labels)
-        # print(f"Testing Score: {testingScore}")
-        # Use the following line to plot the results
 
-        self.outputs.append(f'Logistic Regression, {trainingScore}, {testingScore}')
+        self.outputs.append(f'{classifier_name}, {trainingScore}, {testingScore}')
         self.plot(self.testing_data, clf.predict(self.testing_data),model=clf,classifier_name=classifier_name)
-        import pdb; pdb.set_trace()
 
     def classifyNearestNeighbors(self):
         # TODO: Write code to run a Nearest Neighbors classifier
@@ -114,165 +104,21 @@ class Classifiers():
 
     def classifyLogisticRegression(self):
         # TODO: Write code to run a Logistic Regression classifier
-        avgScore = None
-        bestParams = None
-        for c in [0.1, 0.5, 1, 5 ,10, 50, 100]:
-            total_score = 0
-            for i in range(5):
-                trainIdx = [j for j in range(5) if j != i]
-                validationIdx = i
-
-                trainData = np.vstack([self.foldDatas[j] for j in trainIdx])
-                trainLabels = np.hstack([self.foldLabels[j] for j in trainIdx])
-
-                validationData = self.foldDatas[validationIdx]
-                validationLabels = self.foldLabels[validationIdx]
-
-                clf = LogisticRegression(C=c)
-                clf.fit(trainData, trainLabels)
-                score = clf.score(validationData, validationLabels)
-                total_score += score
-
-            newAvg = total_score / 5
-            print(f'Logistic Regression with C={c} has average score: {newAvg}')
-            if avgScore is None or newAvg > avgScore:
-                avgScore = newAvg
-                bestParams = c
-
-        print(f'Best Logistic Regression params: c={bestParams} with average score: {avgScore}')
-
-        clf = LogisticRegression(C=bestParams)
-        clf.fit(self.training_data, self.training_labels)
-        trainingScore = clf.score(self.training_data, self.training_labels)
-        print(f'Final Logistic Regression Training Score: {trainingScore}')
-        testingScore = clf.score(self.testing_data, self.testing_labels)
-        print(f'Final Logistic Regression Testing Score: {testingScore}')
-
-        self.outputs.append(f'Logistic Regression, {trainingScore}, {testingScore}')
-
-        self.plot(self.testing_data, self.testing_labels, model=clf, classifier_name='Logistic Regression')
+        clf = GridSearchCV(LogisticRegression(), {'C': [0.1, 0.5, 1, 5 ,10, 50, 100]}, cv=None)
+        self.test_clf(clf, 'LogisticRegression')
 
     def classifyDecisionTree(self):
-        # TODO: Write code to run a Logistic Regression classifier
-        avgScore = None
-        bestParams = None
-        for max_depth in range(1, 20, 2):
-            for min_samples_split in range(5, 31, 5):
-                total_score = 0
-                for i in range(5):
-                    trainIdx = [j for j in range(5) if j != i]
-                    validationIdx = i
-
-                    trainData = np.vstack([self.foldDatas[j] for j in trainIdx])
-                    trainLabels = np.hstack([self.foldLabels[j] for j in trainIdx])
-
-                    validationData = self.foldDatas[validationIdx]
-                    validationLabels = self.foldLabels[validationIdx]
-
-                    clf = DecisionTreeClassifier(max_depth=max_depth, min_samples_split=min_samples_split)
-                    clf.fit(trainData, trainLabels)
-                    score = clf.score(validationData, validationLabels)
-                    total_score += score
-
-                newAvg = total_score / 5
-                print(f'Decision Tree with max_depth={max_depth}, min_samples_split={min_samples_split} has average score: {newAvg}')
-                if avgScore is None or newAvg > avgScore:
-                    avgScore = newAvg
-                    bestParams = (max_depth, min_samples_split)
-
-        print(f'Best Decision Tree params: n_neighbors={bestParams[0]}, leaf_size={bestParams[1]} with average score: {avgScore}')
-
-        clf = DecisionTreeClassifier(max_depth=bestParams[0], min_samples_split=bestParams[1])
-        clf.fit(self.training_data, self.training_labels)
-        trainingScore = clf.score(self.training_data, self.training_labels)
-        print(f'Final Decision Tree Training Score: {trainingScore}')
-        testingScore = clf.score(self.testing_data, self.testing_labels)
-        print(f'Final Decision Tree Testing Score: {testingScore}')
-
-        self.outputs.append(f'Decision Tree, {trainingScore}, {testingScore}')
-
-        self.plot(self.testing_data, self.testing_labels, model=clf, classifier_name='Decision Tree')
+        clf = GridSearchCV(DecisionTreeClassifier(), {'max_depth': range(1, 51), 'min_samples_split': range(2, 11)}, cv=None)
+        self.test_clf(clf, 'DecissionTree')
 
     def classifyRandomForest(self):
-        # TODO: Write code to run a Random Forest classifier
-        avgScore = None
-        bestParams = None
-        for max_depth in range(1, 20, 2):
-            for min_samples_split in range(5, 31, 5):
-                total_score = 0
-                for i in range(5):
-                    trainIdx = [j for j in range(5) if j != i]
-                    validationIdx = i
-
-                    trainData = np.vstack([self.foldDatas[j] for j in trainIdx])
-                    trainLabels = np.hstack([self.foldLabels[j] for j in trainIdx])
-
-                    validationData = self.foldDatas[validationIdx]
-                    validationLabels = self.foldLabels[validationIdx]
-
-                    clf = RandomForestClassifier(max_depth=max_depth, min_samples_split=min_samples_split)
-                    clf.fit(trainData, trainLabels)
-                    score = clf.score(validationData, validationLabels)
-                    total_score += score
-
-                newAvg = total_score / 5
-                print(f'Random Forest with max_depth={max_depth}, min_samples_split={min_samples_split} has average score: {newAvg}')
-                if avgScore is None or newAvg > avgScore:
-                    avgScore = newAvg
-                    bestParams = (max_depth, min_samples_split)
-
-        print(f'Best Random Forest params: n_neighbors={bestParams[0]}, leaf_size={bestParams[1]} with average score: {avgScore}')
-
-        clf = RandomForestClassifier(max_depth=bestParams[0], min_samples_split=bestParams[1])
-        clf.fit(self.training_data, self.training_labels)
-        trainingScore = clf.score(self.training_data, self.training_labels)
-        print(f'Final Random Forest Training Score: {trainingScore}')
-        testingScore = clf.score(self.testing_data, self.testing_labels)
-        print(f'Final Random Forest Testing Score: {testingScore}')
-
-        self.outputs.append(f'Random Forest, {trainingScore}, {testingScore}')
-
-        self.plot(self.testing_data, self.testing_labels, model=clf, classifier_name='Random Forest')
+        clf = GridSearchCV(RandomForestClassifier(), {'max_depth': range(1, 6), 'min_samples_split': range(2, 11)}, cv=None)
+        self.test_clf(clf, 'RandomForest')
 
     def classifyAdaBoost(self):
         # TODO: Write code to run a AdaBoost classifier
-        avgScore = None
-        bestParams = None
-        for number_of_estimators in range(10, 71, 10):
-            total_score = 0
-            for i in range(5):
-                trainIdx = [j for j in range(5) if j != i]
-                validationIdx = i
-
-                trainData = np.vstack([self.foldDatas[j] for j in trainIdx])
-                trainLabels = np.hstack([self.foldLabels[j] for j in trainIdx])
-
-                validationData = self.foldDatas[validationIdx]
-                validationLabels = self.foldLabels[validationIdx]
-
-                clf = AdaBoostClassifier(n_estimators=number_of_estimators)
-                clf.fit(trainData, trainLabels)
-                score = clf.score(validationData, validationLabels)
-                total_score += score
-
-            newAvg = total_score / 5
-            print(f'Ada Boost with number_of_estimators={number_of_estimators} has average score: {newAvg}')
-            if avgScore is None or newAvg > avgScore:
-                avgScore = newAvg
-                bestParams = number_of_estimators
-
-        print(f'Best Ada Boost params: number_of_estimators={bestParams} with average score: {avgScore}')
-
-        clf = AdaBoostClassifier(n_estimators=bestParams)
-        clf.fit(self.training_data, self.training_labels)
-        trainingScore = clf.score(self.training_data, self.training_labels)
-        print(f'Final Ada Boost Training Score: {trainingScore}')
-        testingScore = clf.score(self.testing_data, self.testing_labels)
-        print(f'Final Ada Boost Testing Score: {testingScore}')
-
-        self.outputs.append(f'Ada Boost, {trainingScore}, {testingScore}')
-
-        self.plot(self.testing_data, self.testing_labels, model=clf, classifier_name='Ada Boost')
+        clf = GridSearchCV(AdaBoostClassifier(), {'n_estimators': range(10, 71, 10)}, cv=None)
+        self.test_clf(clf, 'AdaBoost')
 
     def plot(self, X, Y, model,classifier_name = ''):
         X1 = X[:, 0]
